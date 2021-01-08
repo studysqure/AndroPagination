f1ef1a6dab772650a5b04cf14085189f34940c6e
Shubham <studysqure.ss@gmail.com>
2021-01-08 20:31:52 +0530

    Initial commit1

---
+++ app/src/main/java/com/sonisony/andropagination/MainData.java
@@ -0,0 +1,11 @@
+package com.sonisony.andropagination;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MainData {
+    private String image;
+          String  name;
+}
